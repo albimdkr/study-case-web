ea22cc0d77f6af612cb0aa8a74c1463d2579fb83
catuser <albimudakar04@gmail.com>
2022-12-19 03:51:19 +0700

    client testimoni

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,23 @@
+// Navbar mobile slider
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function(){
     nav.classList.toggle('slide');
 });
+
+// Swipper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
